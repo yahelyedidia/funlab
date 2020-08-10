a186ac00c1beffc4095818359819007ba0df9eed
galtoledano <>
2020-08-10 11:10:51 +0300

    fasta creator doc

--- fasta_creator.py
+++ fasta_creator.py
@@ -1,13 +1,20 @@
+import sys
 import pandas as pd
 from Bio import SeqIO
 
 
-MATRIX = "/vol/sci/bio/data/yotam.drier/Gal_and_Yahel/cell/sites_groups/stable_met.tsv"
+MATRIX = "/vol/sci/bio/data/yotam.drier/Gal_and_Yahel/cell/sites_groups/{0}.tsv"
 FASTA = "/vol/sci/bio/data/yotam.drier/Gal_and_Yahel/hg38.analysisSet.chroms/chr{0}.fa"
-OUTPUT = "/vol/sci/bio/data/yotam.drier/Gal_and_Yahel/seq_stable_met.fasta"
+OUTPUT = "/vol/sci/bio/data/yotam.drier/Gal_and_Yahel/{0}.fasta"
 
 
 def singal_action(matrix, output_file, cnum):
+    """
+    create fasta format from one chr data only.
+    :param matrix: the relevant CTCF binding sites matrix
+    :param output_file: fata file to write into
+    :param cnum: the chromosome number
+    """
     fasta = SeqIO.read(FASTA.format(cnum), "fasta")
     temp_matrix = matrix[matrix["chr"] == "chr{0}".format(cnum)]
     for site in temp_matrix.iterrows():
@@ -16,9 +23,14 @@ def singal_action(matrix, output_file, cnum):
         output_file.write(">{0}\n{1}\n".format(title, seq))
 
 
-def main():
-    matrix = pd.read_csv(MATRIX, sep="\t")
-    output_file = open(OUTPUT, "w")
+def fasta_creator(matrix_name, output_name):
+    """
+    creates a new fasta file with sequence of CTCF binding sites by matrix
+    :param matrix_name: the matrix name to open
+    :param output_name:  the new output file name.
+    """
+    matrix = pd.read_csv(MATRIX.format(matrix_name), sep="\t")
+    output_file = open(OUTPUT.format(output_name), "w")
     for cnum in range(1, 23):
         singal_action(matrix, output_file, cnum)
     singal_action(matrix, output_file, "X")
@@ -27,4 +39,9 @@ def main():
     print("done")
 
 
-main()
+if __name__ == '__main__':
+    """
+    the arguments files: [1] the matrix name, [2] the output name.
+    """
+    matrix_name, output_name = sys.argv[1], sys.argv[2]
+    fasta_creator(matrix_name, output_name)
